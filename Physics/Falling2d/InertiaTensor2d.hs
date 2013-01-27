78cf1a3d05503e46c0323237d6f69c64cb259b98
sebcrozet <developer@crozet.re>
2013-01-27 03:47:23 +0100

    Fix the special case of 0.0 inertia tensor.

--- Physics/Falling2d/InertiaTensor2d.hs
+++ Physics/Falling2d/InertiaTensor2d.hs
@@ -15,7 +15,7 @@ newtype InertiaTensor2d        = InertiaTensor2d Double
 newtype InverseInertiaTensor2d = InverseInertiaTensor2d Double
 
 instance InertiaTensor InertiaTensor2d InverseInertiaTensor2d Vec1 Proj3 where
-  inverseInertia (InertiaTensor2d inertia) = InverseInertiaTensor2d $ 1.0 / inertia
+  inverseInertia (InertiaTensor2d inertia) = InverseInertiaTensor2d $ if inertia == 0.0 then 0.0 else 1.0 / inertia
 
 instance InverseInertiaTensor InverseInertiaTensor2d Vec1 Proj3 where
   applyToVector (InverseInertiaTensor2d i) v = v &* i
