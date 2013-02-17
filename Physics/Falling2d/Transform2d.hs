8d1470f50531eca7b429211664bbe846b5d0681f
Sébastien Crozet <developer@crozet.re>
2013-02-17 23:41:58 +0100

    Add Unbox instances for Vec2 and Vec1.

--- Physics/Falling2d/Transform2d.hs
+++ Physics/Falling2d/Transform2d.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE TypeSynonymInstances  #-}
+{-# LANGUAGE TypeFamilies          #-}
 
 module Physics.Falling2d.Transform2d
 (
@@ -7,9 +8,12 @@ Transform2d
 )
 where
 
-import Data.Vect.Double.Base
+import Control.Monad(liftM)
+import qualified Data.Vector.Generic as G
+import qualified Data.Vector.Generic.Mutable as M
+import Data.Vector.Unboxed
 import Data.Vect.Double.Util.Dim2
-import Physics.Falling.Math.Transform
+import Physics.Falling.Math.Transform hiding(Vector)
 import Physics.Falling2d.Vec1
 
 type Transform2d = Proj3
@@ -39,3 +43,46 @@ instance Rotatable Transform2d Vec1 where
 
 instance Transform       Transform2d Vec2
 instance TransformSystem Transform2d Vec2 Vec1
+
+newtype instance MVector s Vec2 = MV_Vec2 (MVector s (Double, Double))
+newtype instance Vector    Vec2 = V_Vec2  (Vector    (Double, Double))
+instance Unbox Vec2
+
+instance M.MVector MVector Vec2 where
+  {-# INLINE basicLength #-}
+  {-# INLINE basicUnsafeSlice #-}
+  {-# INLINE basicOverlaps #-}
+  {-# INLINE basicUnsafeNew #-}
+  {-# INLINE basicUnsafeReplicate #-}
+  {-# INLINE basicUnsafeRead #-}
+  {-# INLINE basicUnsafeWrite #-}
+  {-# INLINE basicClear #-}
+  {-# INLINE basicSet #-}
+  {-# INLINE basicUnsafeCopy #-}
+  {-# INLINE basicUnsafeGrow #-}
+  basicLength (MV_Vec2 v)                   = M.basicLength v
+  basicUnsafeSlice i n (MV_Vec2 v)          = MV_Vec2 $ M.basicUnsafeSlice i n v
+  basicOverlaps (MV_Vec2 v1) (MV_Vec2 v2)   = M.basicOverlaps v1 v2
+  basicUnsafeNew n                          = MV_Vec2 `liftM` M.basicUnsafeNew n
+  basicUnsafeReplicate n (Vec2 x y)         = MV_Vec2 `liftM` M.basicUnsafeReplicate n (x, y)
+  basicUnsafeRead (MV_Vec2 v) i             = uncurry Vec2 `liftM` M.basicUnsafeRead v i
+  basicUnsafeWrite (MV_Vec2 v) i (Vec2 x y) = M.basicUnsafeWrite v i (x, y)
+  basicClear (MV_Vec2 v)                    = M.basicClear v
+  basicSet (MV_Vec2 v) (Vec2 x y)           = M.basicSet v (x, y)
+  basicUnsafeCopy (MV_Vec2 v1) (MV_Vec2 v2) = M.basicUnsafeCopy v1 v2
+  basicUnsafeGrow (MV_Vec2 v) n             = MV_Vec2 `liftM` M.basicUnsafeGrow v n
+
+instance G.Vector Vector Vec2 where
+  {-# INLINE basicUnsafeFreeze #-}
+  {-# INLINE basicUnsafeThaw #-}
+  {-# INLINE basicLength #-}
+  {-# INLINE basicUnsafeSlice #-}
+  {-# INLINE basicUnsafeIndexM #-}
+  {-# INLINE elemseq #-}
+  basicUnsafeFreeze (MV_Vec2 v)           = V_Vec2 `liftM` G.basicUnsafeFreeze v
+  basicUnsafeThaw (V_Vec2 v)              = MV_Vec2 `liftM` G.basicUnsafeThaw v
+  basicLength (V_Vec2 v)                  = G.basicLength v
+  basicUnsafeSlice i n (V_Vec2 v)         = V_Vec2 $ G.basicUnsafeSlice i n v
+  basicUnsafeIndexM (V_Vec2 v) i          = uncurry Vec2 `liftM` G.basicUnsafeIndexM v i
+  basicUnsafeCopy (MV_Vec2 mv) (V_Vec2 v) = G.basicUnsafeCopy mv v
+  elemseq _                               = seq
--- Physics/Falling2d/Vec1.hs
+++ Physics/Falling2d/Vec1.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeFamilies          #-}
 
 module Physics.Falling2d.Vec1
 (
@@ -6,8 +7,12 @@ Vec1(..)
 )
 where
 
-import Data.Vect.Double.Base
-import Physics.Falling.Math.Transform
+import Control.Monad(liftM)
+import qualified Data.Vector.Generic as G
+import qualified Data.Vector.Generic.Mutable as M
+import Data.Vector.Unboxed
+import Physics.Falling.Math.Transform hiding(Vector)
+import qualified Physics.Falling.Math.Transform as T
 
 data Vec1 = Vec1 !Double
             deriving(Show)
@@ -15,11 +20,11 @@ data Vec1 = Vec1 !Double
 instance Eq Vec1 where
   Vec1 a == Vec1 b = a == b
 
-instance Vector       Vec1 where  
+instance T.Vector Vec1 where
   scalarMul s (Vec1 x) = Vec1 $ s * x
   mapVec    f (Vec1 x) = Vec1 $ f x
 
-instance AbelianGroup Vec1 where  
+instance AbelianGroup Vec1 where
   (Vec1 x1) &+ (Vec1 x2) = Vec1 $ x1 + x2
   (Vec1 x1) &- (Vec1 x2) = Vec1 $ x1 - x2
   neg (Vec1 x)           = Vec1 $ -x
@@ -30,3 +35,46 @@ instance DotProd Vec1 where
 
 instance PerpProd Vec2 Vec1 where
   (Vec2 x y) `perp` (Vec2 x' y') = Vec1 $ x * y' - y * x'
+
+newtype instance MVector s Vec1 = MV_Vec1 (MVector s Double)
+newtype instance Vector    Vec1 = V_Vec1  (Vector    Double)
+instance Unbox Vec1
+
+instance M.MVector MVector Vec1 where
+  {-# INLINE basicLength #-}
+  {-# INLINE basicUnsafeSlice #-}
+  {-# INLINE basicOverlaps #-}
+  {-# INLINE basicUnsafeNew #-}
+  {-# INLINE basicUnsafeReplicate #-}
+  {-# INLINE basicUnsafeRead #-}
+  {-# INLINE basicUnsafeWrite #-}
+  {-# INLINE basicClear #-}
+  {-# INLINE basicSet #-}
+  {-# INLINE basicUnsafeCopy #-}
+  {-# INLINE basicUnsafeGrow #-}
+  basicLength (MV_Vec1 v)                   = M.basicLength v
+  basicUnsafeSlice i n (MV_Vec1 v)          = MV_Vec1 $ M.basicUnsafeSlice i n v
+  basicOverlaps (MV_Vec1 v1) (MV_Vec1 v2)   = M.basicOverlaps v1 v2
+  basicUnsafeNew n                          = MV_Vec1 `liftM` M.basicUnsafeNew n
+  basicUnsafeReplicate n (Vec1 x)           = MV_Vec1 `liftM` M.basicUnsafeReplicate n x
+  basicUnsafeRead (MV_Vec1 v) i             = Vec1 `liftM` M.basicUnsafeRead v i
+  basicUnsafeWrite (MV_Vec1 v) i (Vec1 x)   = M.basicUnsafeWrite v i x
+  basicClear (MV_Vec1 v)                    = M.basicClear v
+  basicSet (MV_Vec1 v) (Vec1 x)             = M.basicSet v x
+  basicUnsafeCopy (MV_Vec1 v1) (MV_Vec1 v2) = M.basicUnsafeCopy v1 v2
+  basicUnsafeGrow (MV_Vec1 v) n             = MV_Vec1 `liftM` M.basicUnsafeGrow v n
+
+instance G.Vector Vector Vec1 where
+  {-# INLINE basicUnsafeFreeze #-}
+  {-# INLINE basicUnsafeThaw #-}
+  {-# INLINE basicLength #-}
+  {-# INLINE basicUnsafeSlice #-}
+  {-# INLINE basicUnsafeIndexM #-}
+  {-# INLINE elemseq #-}
+  basicUnsafeFreeze (MV_Vec1 v)           = V_Vec1 `liftM` G.basicUnsafeFreeze v
+  basicUnsafeThaw (V_Vec1 v)              = MV_Vec1 `liftM` G.basicUnsafeThaw v
+  basicLength (V_Vec1 v)                  = G.basicLength v
+  basicUnsafeSlice i n (V_Vec1 v)         = V_Vec1 $ G.basicUnsafeSlice i n v
+  basicUnsafeIndexM (V_Vec1 v) i          = Vec1 `liftM` G.basicUnsafeIndexM v i
+  basicUnsafeCopy (MV_Vec1 mv) (V_Vec1 v) = G.basicUnsafeCopy mv v
+  elemseq _                               = seq
